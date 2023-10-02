60e8584c4fcbab78273f4d856445744cdd2d5647
mestebenet <103604038+mestebenet@users.noreply.github.com>
2023-10-02 11:55:21 -0300

    Update project-answers.sql

--- parte-1/project/project-answers.sql
+++ parte-1/project/project-answers.sql
@@ -7,8 +7,8 @@ with Valores_en_dolares as (
  to_char(date, 'YYYY-MM') AS mes_y_anio,	
  order_number,
  sale/fx_rate_usd_peso as Sale_en_dolares,
- coalesce(promotion,0)*fx_rate_usd_peso as Promotion_en_dolares,
- coalesce(credit,0)*fx_rate_usd_peso as Creditos_en_dolares,
+ coalesce(promotion,0)/fx_rate_usd_peso as Promotion_en_dolares,
+ coalesce(credit,0)/fx_rate_usd_peso as Creditos_en_dolares,
  coalesce(tax,0)/fx_rate_usd_peso as Tax_en_dolares,
  product_cost_usd/fx_rate_usd_peso as Costo_en_dolares
  FROM stg.order_line_sale ol
@@ -18,7 +18,7 @@ left join stg.monthly_average_fx_rate fx on date_trunc('month',date)=fx.month
  select 
  mes_y_anio,
  sum(Sale_en_dolares) as sales_usd,
- sum(Sale_en_dolares-Costo_en_dolares) as net_sales_usd, 
+ sum(Sale_en_dolares-Promotion_en_dolares) as net_sales_usd, 
  sum(Sale_en_dolares-Costo_en_dolares-Promotion_en_dolares-Creditos_en_dolares-Tax_en_dolares)as margin_usd
  from Valores_en_dolares
  group by mes_y_anio;
@@ -47,16 +47,24 @@ left join stg.monthly_average_fx_rate fx on date_trunc('month',date)=fx.month
  group by mes_y_anio,category
 
 -- - ROI por categoria de producto. ROI = ventas netas / Valor promedio de inventario (USD)
--- falta terminar
-select s.date,
-s.sale as ventas_netas,
-((inv.initial+inv.final)/2)*product_cost_usd as Costo_inventario_promedio
+--Terminar sacando el costo del inventario promedio y luego roi.
+	with Ventas as (	
+	select 
+	to_char(s.date, 'YYYY-MM') AS mes_y_anio,
+	category, 
+sum(s.sale-coalesce (s.promotion,0)) as ventas_netas,
+avg((inv.initial+inv.final)/2)as inventario_promedio
 from stg.order_line_sale s
 left join stg.cost c on c.product_code=s.product
+left join stg.product_master pm on s.product = pm.product_code
 left join stg.inventory inv 
 on s.product = inv.item_id
 and s.store = inv.store_id
 and s.date = inv.date
+group by mes_y_anio, category
+order by mes_y_anio, category
+		)
+		 
 
 
 -- - AOV (Average order value), valor promedio de la orden. (USD)
