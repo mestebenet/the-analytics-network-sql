fd7e2b8fe50ec45b8be375ab3ec2f962096ddba2
mestebenet <103604038+mestebenet@users.noreply.github.com>
2023-10-24 10:37:20 -0300

    Update respuestas-2.sql

--- parte-2/homework/respuestas-2.sql
+++ parte-2/homework/respuestas-2.sql
@@ -545,10 +545,36 @@ r.quantity *(
 - Fecha del año anterior (date, ejemplo: 2021-01-01 para la fecha 2022-01-01) `date_ly`
 - Nota: En general una tabla date es creada para muchos años mas (minimo 10), en este caso vamos a realizarla para el 2022 y 2023 nada mas.. 
 */
-
+SELECT 
+    to_char(date_c, 'yyyymmdd')::int as Date_ID,
+    cast(date_c as date) as date,
+    extract(month from date_c) as month,
+    extract(year from date_c) as year,
+    to_char(date_c, 'day') as weekday,
+	
+    CASE WHEN to_char(date_c, 'day') like '%saturday%' THEN 'True' 
+		WHEN (to_char(date_c, 'day'))like '%sunday%' THEN 'True' 
+		ELSE 'False' END as is_weekend,
+	
+	to_char(date_c, 'month') as month_label,
+	case when date_c BETWEEN '20220201' AND '20230131' then '2022' 	else '2023' end as fiscal_year,
+	case when date_c BETWEEN '20220201' AND '20230131' then 'FY2022' 	else 'FY2023' end as fiscal_year_label,
+	
+	case when extract(month from date_c) in (2,3,4) then 'Q1'
+		when extract(month from date_c) in (5,6,7) then 'Q2'
+		when extract(month from date_c) in (8,9,10) then 'Q3'
+		when extract(month from date_c) in (11,12,1) then 'Q4' end as fiscal_quarter_label,
+		
+	CAST( date_c - interval '1 year' AS date) AS date_ly
+	
+FROM (
+    SELECT cast('2022-02-01' as date) + (n || ' day')::interval as date_c
+    FROM generate_series(0, 730) n
+) dd;
 -- ## Semana 4 - Parte B
 
 -- 1. Calcular el crecimiento de ventas por tienda mes a mes, con el valor nominal y el valor % de crecimiento. Utilizar self join.
+
 with ventas_mes as (
 select 
 		cast(date_trunc('month', s.date) as date) as mes,
@@ -578,7 +604,8 @@ order by
 	v1.store, v1.mes asc
 
 -- 2. Hacer un update a la tabla de stg.product_master agregando una columna llamada brand, con la marca de cada producto con la primer letra en mayuscula. Sabemos que las marcas que tenemos son: Levi's, Tommy Hilfiger, Samsung, Phillips, Acer, JBL y Motorola. En caso de no encontrarse en la lista usar Unknown.
-Alter Table stg.product_master
+
+	Alter Table stg.product_master
 add column brand varchar(255)
 update stg.product_master
 set brand=
