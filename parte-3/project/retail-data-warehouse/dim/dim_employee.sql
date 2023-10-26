7885e03ac3136a20994134a577e37156f08cfd1b
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-10-26 21:53:52 +0200

    Update dim_employee.sql

--- parte-3/project/retail-data-warehouse/dim/dim_employee.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_employee.sql
@@ -8,12 +8,14 @@ CREATE TABLE IF NOT EXISTS dim.employee
                           	name varchar(255),
                           	surname varchar(255),
                           	start_date date,
-                          	end_date date, 
+                          	end_date date,
+                            duration smallint,
                           	phone varchar(30), 
                           	country varchar(30), 
                           	province varchar(30), 
                           	store_id smallint, 
                           	position varchar(30),
+                            active boolean,
                             constraint fk_store_id
                             		foreign key (store_id)
                             		references dim.store_master(store_id)
