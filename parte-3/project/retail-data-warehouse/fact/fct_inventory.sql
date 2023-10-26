9600f27b9b50cddf63291350621264a299243e39
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-10-26 21:01:45 +0200

    Create fct_inventory.sql

---
+++ parte-3/project/retail-data-warehouse/fact/fct_inventory.sql
@@ -0,0 +1,18 @@
+-- Table: dim.inventory
+
+DROP TABLE IF EXISTS dim.inventory;
+
+CREATE TABLE IF NOT EXISTS dim.inventory
+(
+		 		                      date        DATE
+                            , store_id    SMALLINT
+                            , item_id     VARCHAR(10)
+                            , initial     SMALLINT
+                            , final       SMALLINT
+                            , constraint fk_store_id
+                              		foreign key (store_id)
+                              		references dim.store_master(store_id)
+                            , constraint fk_item_id
+                              		foreign key (item_id)
+                              		references dim.product_master(product_code)
+);
