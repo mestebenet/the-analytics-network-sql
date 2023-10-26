03e4b83e1d08899ea15dc0e8d8c9f6186e09340e
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-10-26 20:19:34 +0200

    Create dim_employee.sql

---
+++ parte-3/project/retail-data-warehouse/dim/dim_employee.sql
@@ -0,0 +1,21 @@
+-- Table: dim.employee
+
+DROP TABLE IF EXISTS dim.employee;
+
+CREATE TABLE IF NOT EXISTS dim.employee
+(
+                            id serial primary key,
+                          	name varchar(255),
+                          	surname varchar(255),
+                          	start_date date,
+                          	end_date date, 
+                          	phone varchar(30), 
+                          	country varchar(30), 
+                          	province varchar(30), 
+                          	store_id smallint, 
+                          	position varchar(30),
+                            constraint fk_store_id
+                            		foreign key (store_id)
+                            		references dim.store_master(store_id)
+);
+
