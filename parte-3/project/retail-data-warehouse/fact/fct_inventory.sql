9f4bd90c238134b6e08ea23b1afcf69f043fc16c
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-10-26 21:36:53 +0200

    Update fct_inventory.sql

--- parte-3/project/retail-data-warehouse/fact/fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/fact/fct_inventory.sql
@@ -4,11 +4,12 @@ DROP TABLE IF EXISTS dim.inventory;
 
 CREATE TABLE IF NOT EXISTS dim.inventory
 (
-		 		                      date        DATE
+		 	      date        DATE
                             , store_id    SMALLINT
                             , item_id     VARCHAR(10)
                             , initial     SMALLINT
                             , final       SMALLINT
+			    , PRIMARY KEY (date, store_id, item_id)
                             , constraint fk_store_id
                               		foreign key (store_id)
                               		references dim.store_master(store_id)
