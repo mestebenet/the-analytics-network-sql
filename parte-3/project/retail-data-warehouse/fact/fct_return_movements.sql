7626dd7c53b8cd3af389d77602876c97195d6767
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-10-26 21:12:17 +0200

    Create fct_return_movements.sql

---
+++ parte-3/project/retail-data-warehouse/fact/fct_return_movements.sql
@@ -0,0 +1,20 @@
+-- Table: dim.return_movements
+
+DROP TABLE IF EXISTS dim.return_movements;
+
+CREATE TABLE IF NOT EXISTS dim.return_movements
+(
+                                            order_id character varying(255),
+                                            return_id character varying(255),
+                                            item character varying(255),
+                                            quantity integer,
+                                            movement_id integer,
+                                            from_location character varying(255),
+                                            to_location character varying(255),
+                                            received_by character varying(255),
+                                            date date,
+                                            constraint fk_item_id
+                                                  foreign key (item)
+                                              		references dim.product_master(product_code)
+);
+
