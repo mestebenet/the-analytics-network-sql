316ecaaa01778c231fdf6cb3027424232e6686d1
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-10-26 21:34:35 +0200

    Update dim_supplier_product.sql

--- parte-3/project/retail-data-warehouse/dim/dim_supplier_product.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_supplier_product.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS dim.supplier_product;
 
 CREATE TABLE IF NOT EXISTS dim.supplier_product
 (
-                            product_id varchar(255) PRIMARY KEY,
+                            product_id varchar(255),
                             name varchar(255),
                             is_primary boolean
                             , constraint fk_product_id
