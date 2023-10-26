9a8c04e67ffd56e5a8a41d6f43bf5656be2fe14d
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-10-26 20:06:10 +0200

    Update dim_product_master.sql

--- parte-3/project/retail-data-warehouse/dim/dim_product_master.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_product_master.sql
@@ -4,9 +4,21 @@ DROP TABLE IF EXISTS dim.product_master;
 
 CREATE TABLE IF NOT EXISTS dim.product_master
 (
-    product_id varchar(10) PRIMARY KEY,
-    cost_usd numeric,
-	constraint fk_product_id_cost
-		foreign key (product_id)
-		references dim.product_master(product_code)
+                              product_code    varchar(10) PRIMARY KEY
+                            , name            varchar(255)
+                            , category        varchar(255)
+                            , subcategory     varchar(255)
+                            , subsubcategory  varchar(255)
+                            , material        varchar(255)
+                            , color           varchar(255)
+                            , origin          varchar(255)
+                            , ean             bigint
+                            , is_active       boolean
+                            , has_bluetooth   boolean
+                            , size            varchar(255)
 );
+
+
+                 
+
+                 );
